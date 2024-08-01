a82e7901873f4162cb2a7c5ac8c61b0f9e07aea9
harsh02it <harshitsavani@gmail.com>
2024-08-01 17:33:10 -0400

    Added a container for the game board

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,20 @@
 function App() {
   return (
-    <header>
-      <h1>React Tic-Tac-Toe</h1>
-    </header>
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <li>
+            <span className="player-name">Player 1</span>
+            <span className="player-symbol">X</span>
+          </li>
+          <li>
+            <span className="player-name">Player 2</span>
+            <span className="player-symbol">O</span>
+          </li>
+        </ol>
+        Game Board
+      </div>
+    </main>
   );
 }
 
